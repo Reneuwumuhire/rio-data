d35df3f9ff084b85b5ee0f95ee1730371f592e8e
guillaume250 <guillaumesayinzoga@gmail.com>
2021-01-05 21:33:31 -0500

    adds statBox design

--- src/ui-kit/StatBox/index.js
+++ src/ui-kit/StatBox/index.js
@@ -1,22 +1,48 @@
 import React from "react";
 import "./style.css";
 
-const StatBox = ({ title, stats }) => {
+const StatBox = ({ title, stats, valColor }) => {
   const styles = {
-    text: {}
+    container: {
+      border: "solid 1px #2F856E",
+      display: "flex",
+      flexDirection: "column",
+      padding: 20,
+      margin: 10,
+      height: "fit-content",
+      width: "fit-content",
+      borderRadius: 4
+    },
+    title: {
+      color: "#003300",
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 10
+    },
+    stat: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      color: "#003300",
+      fontSize: 16,
+      fontWeight: "bold",
+      marginTop: 10
+    },
+    statValue: { marginLeft: 10, color: valColor && valColor }
   };
   return (
-    <>
-      <div>{title}</div>
+    <div style={styles.container}>
+      <div style={styles.title}>{title}</div>
       {stats &&
         stats.map(({ statName, value }) => {
           return (
-            <div>
-              {statName}: {value}
+            <div style={styles.stat}>
+              <div>{statName}:</div>
+              <div style={styles.statValue}>{value}</div>
             </div>
           );
         })}
-    </>
+    </div>
   );
 };
 
